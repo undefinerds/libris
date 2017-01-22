7b1979204111d7b8c033d6099cbec97b3644c748
Jhinel Arcaya <proyectoamicoro@gmail.com>
2017-01-22 14:36:59 -0400

    Fixing minor bugs

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-  const mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL(`file://{__dirname}/public/index.html`);
+  var mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL(`file:///${__dirname}/public/index.html`);
   mainWindow.webContents.on('did-finish-load', () => {
     mainWindow.show();
     mainWindow.focus();
