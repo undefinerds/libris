c6eeaa7e9ab13365860bb81063ac18a160929c83
Jhinel Arcaya <proyectoamicoro@gmail.com>
2017-02-17 13:36:04 -0400

    packaging working

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL(`file:///${process.cwd()}/public/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/public/index.html`);
   mainWindow.webContents.on('did-finish-load', () => {
     mainWindow.show();
     mainWindow.focus();
+    mainWindow.openDevTools();
   });
 
   mainWindow.on('closed', function() {
